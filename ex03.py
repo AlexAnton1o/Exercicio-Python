0cc9c70a11297eac742510b52effd0130f917c78
Alex <142420860+AlexAnton1o@users.noreply.github.com>
2023-09-20 20:48:51 -0300

    Exercícios simples

---
+++ ex03.py
@@ -0,0 +1,4 @@
+produto = int(input('Digite o valor do produto'))
+desconto = produto - (produto*0.21)
+adicional = produto + (produto*0.25)
+print(f'O valor do produto com 21% de desconto é {desconto}, e o valor do produto com 25% a mais é {adicional}')
